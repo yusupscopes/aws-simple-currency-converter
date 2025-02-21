58a4a8e26e318622a2e869b204c8448b8df6eef4
yusupscopes <yusupmaulana950@gmail.com>
2025-02-21 13:58:31 +0700

    feat: fastAPI init

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+handler = Mangum(app)
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello from currency converter"}
